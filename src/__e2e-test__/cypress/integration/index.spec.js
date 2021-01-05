a5eb34d4bce3b09e05be1cbd882fe2a5f609f925
santiagoep <hola@santiagoep.com>
2021-01-04 23:58:58 -0500

    Adding Integration and E2E Testing

--- src/__e2e-test__/cypress/integration/index.spec.js
+++ src/__e2e-test__/cypress/integration/index.spec.js
@@ -1,13 +1,18 @@
 context('Home', () => {
   it('Go To Home', () => {
     cy.visit('/');
+
     cy.get('.product').should(($product) => {
       expect($product).to.have.length(10);
     });
+
+    // Should search
     cy.get('#query-text-filters').type('Chaqueta Negra');
     cy.get('.product__price').should(($price) => {
       expect($price).to.have.length(3);
     });
+
+    // Should filter price from lowest to highest
     cy.get('#sort-filters').select('price_from_lowest_to_highest');
     cy.get('.product__price')
       .first()
@@ -19,6 +24,8 @@ context('Home', () => {
       .then(($productPrice) => {
         expect($productPrice.text()).to.include('212.900');
       });
+
+    // Should filter prices from highest to lowest
     cy.get('#sort-filters').select('price_from_highest_to_lowest');
     cy.get('.product__price')
       .first()
@@ -30,6 +37,8 @@ context('Home', () => {
       .then(($productPrice) => {
         expect($productPrice.text()).to.include('212.900');
       });
+
+    // Should clear search
     cy.get('#query-text-filters').clear();
     cy.get('.product').should(($product) => {
       expect($product).to.have.length(10);
@@ -39,13 +48,21 @@ context('Home', () => {
       .then(($productPrice) => {
         expect($productPrice.text()).to.include('89.900');
       });
-    cy.get('.product__price').first().click();
+
+    // Should go to product detail
+    cy.get('.product').first().click();
+    cy.url().should('include', '/products/');
     cy.get('[data-testid=product-detail__category-name]').should('be.visible');
     cy.get('[data-testid=product-detail__name]').should('be.visible');
     cy.get('[data-testid=product-detail__description]').should('be.visible');
     cy.get('[data-testid=product-detail__go-back]').should('be.visible');
+
+    // Should go back to home
     cy.get('[data-testid=product-detail__go-back]').click();
-    cy.get('.product__price').first().click();
+
+    // Should click on logo and back to home
+    cy.get('.product').first().click();
     cy.get('[alt="Logo Platzi"]').click();
+    cy.url().should('include', '/');
   });
 });
